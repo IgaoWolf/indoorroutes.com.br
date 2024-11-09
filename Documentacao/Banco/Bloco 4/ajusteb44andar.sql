52d93fdce8a8872efd00d1ba9de1e2efdcee2d05
Igor Augusto Wolf <igaowolf@gmail.com>
2024-11-09 19:27:37 -0300

    4 andar - documentação e banco de dados

---
+++ Documentacao/Banco/Bloco 4/ajusteb44andar.sql
@@ -0,0 +1,128 @@
+-- Salas do Bloco 4, Andar 4
+INSERT INTO waypoints (nome, descricao, tipo, bloco_id, andar_id, coordenadas) VALUES
+('Sala 4401', 'Sala 4401 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50738764, -24.94575153), 4326)),
+('Sala 4402', 'Sala 4402 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50756735, -24.94581172), 4326)),
+('Sala 4403', 'Sala 4403 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50759484, -24.94580017), 4326)),
+('Sala 4404', 'Sala 4404 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50777186, -24.94572721), 4326)),
+('Sala 4405', 'Sala 4405 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50779802, -24.94571322), 4326)),
+('Sala 4406', 'Sala 4406 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50786306, -24.94555089), 4326)),
+('Sala 4407', 'Sala 4407 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50785099, -24.94552657), 4326)),
+('Sala 4464', 'Sala 4464 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50776851, -24.94536606), 4326)),
+('Sala 4409', 'Sala 4409 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50775577, -24.94534295), 4326)),
+('Sala 4465', 'Sala 4465 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50757405, -24.94528398), 4326)),
+('Sala 4411', 'Sala 4411 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50754991, -24.94529431), 4326)),
+('Sala 4412', 'Sala 4412 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50737154, -24.94536849), 4326)),
+('Sala 4413', 'Sala 4413 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.5074114, -24.94538065), 4326)),
+('Sala 4414', 'Sala 4414 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50728169, -24.94554359), 4326)),
+('Sala 4415', 'Sala 4415 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50729175, -24.9455673), 4326)),
+('Sala 4416', 'Sala 4416 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50737557, -24.94572903), 4326)),
+('Sala 4417', 'Sala 4417 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50775577, -24.94569316), 4326)),
+('Sala 4418', 'Sala 4418 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50781545, -24.94554298), 4326)),
+('Sala 4419', 'Sala 4419 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50738898, -24.94540071), 4326)),
+('Sala 4420', 'Sala 4420 no 4º andar', 'sala', 4, 4, ST_SetSRID(ST_MakePoint(-53.50732796, -24.94555393), 4326));
+
+-- Entradas do Bloco 4, Andar 4
+INSERT INTO waypoints (nome, descricao, tipo, bloco_id, andar_id, coordenadas) VALUES
+('Saída da Escada para o 4º Andar - Granvia', 'Saída da escada do 3º andar para o 4º andar - acesso Granvia', 'escada', 4, 4, ST_SetSRID(ST_MakePoint(-53.50783624, -24.94560622), 4326)),
+('Saída da Escada para o 4º Andar - Estacionamento', 'Saída da escada do 3º andar para o 4º andar - acesso Estacionamento', 'escada', 4, 4, ST_SetSRID(ST_MakePoint(-53.50730717, -24.94548158), 4326));
+
+-- Corredores do Bloco 4, Andar 4
+INSERT INTO waypoints (nome, descricao, tipo, bloco_id, andar_id, coordenadas) VALUES
+('Corredor 1', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50786894, -24.94562026), 4326)),
+('Corredor 2', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50785471, -24.94565723), 4326)),
+('Corredor 3', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50785376, -24.94557555), 4326)),
+('Corredor 4', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50784144, -24.94554803), 4326)),
+('Corredor 5', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50782437, -24.94551794), 4326)),
+('Corredor 6', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50780161, -24.94548011), 4326)),
+('Corredor 7', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50777885, -24.94543712), 4326)),
+('Corredor 8', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50775799, -24.94540272), 4326)),
+('Corredor 9', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50773617, -24.94537349), 4326)),
+('Corredor 10', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50770012, -24.94535147), 4326)),
+('Corredor 11', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50765586, -24.94533444), 4326)),
+('Corredor 12', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50760825, -24.94532167), 4326)),
+('Corredor 13', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50755394, -24.94532167), 4326)),
+('Corredor 14', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50750767, -24.94533323), 4326)),
+('Corredor 15', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.5074588, -24.94535027), 4326)),
+('Corredor 16', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.5074166, -24.94536919), 4326)),
+('Corredor 17', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50737629, -24.94538638), 4326)),
+('Corredor 18', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50733315, -24.94540315), 4326)),
+('Corredor 19', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.5073009, -24.94542551), 4326)),
+('Corredor 20', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50728573, -24.94545044), 4326)),
+('Corredor 21', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50727672, -24.94548183), 4326)),
+('Corredor 22', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50728905, -24.94551407), 4326)),
+('Corredor 23', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50730248, -24.94554663), 4326)),
+('Corredor 24', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50731924, -24.94557399), 4326)),
+('Corredor 25', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50733198, -24.945605), 4326)),
+('Corredor 26', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.5073521, -24.94563783), 4326)),
+('Corredor 27', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50737154, -24.94567553), 4326)),
+('Corredor 28', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50739501, -24.94570167), 4326)),
+('Corredor 29', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50741848, -24.94572964), 4326)),
+('Corredor 30', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50745335, -24.94575031), 4326)),
+('Corredor 31', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50749627, -24.94576551), 4326)),
+('Corredor 32', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50755058, -24.94577342), 4326)),
+('Corredor 33', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50758746, -24.94576916), 4326)),
+('Corredor 34', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50764044, -24.945757), 4326)),
+('Corredor 35', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50767061, -24.94574666), 4326)),
+('Corredor 36', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50770535, -24.94573161), 4326)),
+('Corredor 37', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50773712, -24.94572258), 4326)),
+('Corredor 38', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50776178, -24.94571054), 4326)),
+('Corredor 39', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50780066, -24.94569421), 4326)),
+('Corredor 40', 'Corredor no 4º andar do Bloco 4', 'corredor', 4, 4, ST_SetSRID(ST_MakePoint(-53.50783527, -24.94567916), 4326));
+
+-- Destinos do tipo "escada"
+INSERT INTO destinos (nome, descricao, waypoint_id) VALUES
+('Saida da escada do 3 andar para o 4 - Granvia', 'Saída da escada do 3º andar para o 4º andar pela Granvia', (SELECT id FROM waypoints WHERE nome = 'Saida da escada do 3 andar para o 4 - Granvia' AND bloco_id = 4 AND andar_id = 4)),
+('Saida da escada do 3 andar para o 4 - Estacionamento', 'Saída da escada do 3º andar para o 4º andar pelo Estacionamento', (SELECT id FROM waypoints WHERE nome = 'Saida da escada do 3 andar para o 4 - Estacionamento' AND bloco_id = 4 AND andar_id = 4));
+
+-- Destinos do tipo "sala"
+INSERT INTO destinos (nome, descricao, waypoint_id) VALUES
+('Sala 4401', 'Sala 4401 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4401' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4402', 'Sala 4402 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4402' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4403', 'Sala 4403 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4403' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4404', 'Sala 4404 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4404' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4405', 'Sala 4405 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4405' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4406', 'Sala 4406 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4406' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4407', 'Sala 4407 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4407' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4464', 'Sala 4464 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4464' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4409', 'Sala 4409 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4409' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4465', 'Sala 4465 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4465' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4411', 'Sala 4411 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4411' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4412', 'Sala 4412 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4412' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4413', 'Sala 4413 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4413' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4414', 'Sala 4414 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4414' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4415', 'Sala 4415 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4415' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4416', 'Sala 4416 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4416' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4417', 'Sala 4417 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4417' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4418', 'Sala 4418 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4418' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4419', 'Sala 4419 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4419' AND bloco_id = 4 AND andar_id = 4)),
+('Sala 4420', 'Sala 4420 no 4º andar do Bloco 4', (SELECT id FROM waypoints WHERE nome = 'Sala 4420' AND bloco_id = 4 AND andar_id = 4));
+
+
+411 - entrada escada para o 4 andar - estacionamento
+465 - saida do 3 andar para o 4 - estacionamento
+
+
+409 - entrada escada para o 4 andar - granvia
+464 - saida do 3 andar para o 4 - granvia
+
+-- Inserir conexão entre Escadaria bloco 4 estacionamento (3º andar para 4º andar) e vice-versa
+INSERT INTO conexoes (waypoint_origem_id, waypoint_destino_id, distancia)
+SELECT 411, 465, ST_Distance(w1.coordenadas, w2.coordenadas)
+FROM waypoints w1, waypoints w2
+WHERE w1.id = 411 AND w2.id = 465;
+
+INSERT INTO conexoes (waypoint_origem_id, waypoint_destino_id, distancia)
+SELECT 465, 411, ST_Distance(w1.coordenadas, w2.coordenadas)
+FROM waypoints w1, waypoints w2
+WHERE w1.id = 465 AND w2.id = 411;
+
+-- Inserir conexão entre Escadaria bloco 4 acesso granvia (3º andar para 4º andar) e vice-versa
+INSERT INTO conexoes (waypoint_origem_id, waypoint_destino_id, distancia)
+SELECT 409, 464, ST_Distance(w1.coordenadas, w2.coordenadas)
+FROM waypoints w1, waypoints w2
+WHERE w1.id = 409 AND w2.id = 464;
+
+INSERT INTO conexoes (waypoint_origem_id, waypoint_destino_id, distancia)
+SELECT 464, 409, ST_Distance(w1.coordenadas, w2.coordenadas)
+FROM waypoints w1, waypoints w2
+WHERE w1.id = 464 AND w2.id = 409;
