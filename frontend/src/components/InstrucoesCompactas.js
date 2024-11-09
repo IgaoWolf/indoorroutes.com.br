7be412a041f44b33d4dd962e262462be2b0d5125
Rafael Pagliari <rafaelpagliari15@hotmail.com>
2024-11-09 18:48:38 -0300

    Expansao das instrucoes

--- frontend/src/components/InstrucoesCompactas.js
+++ frontend/src/components/InstrucoesCompactas.js
@@ -1,29 +1,34 @@
 import React, { useState } from 'react';
-import { FaArrowDown, FaArrowUp } from 'react-icons/fa';
+import { FaArrowLeft } from 'react-icons/fa';
 import '../styles/InstrucoesCompactas.css';
 
-const InstrucoesCompactas = ({ instrucoes }) => {
+const InstrucoesCompactas = ({ instrucoes, onBack }) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
+  // Alterna a expansão das instruções
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
   };
 
   return (
     <div className="instrucoes-compactas">
-      <div className="instrucao-principal">
+      <div className="header">
+        <button className="back-arrow" onClick={onBack}>
+          <FaArrowLeft />
+        </button>
+        <p className="trajeto-titulo">Ver trajeto</p>
+      </div>
+      <div className="linha-separadora"></div>
+      <div className="instrucao-texto" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <span>{instrucoes[0]}</span>
         <button className="toggle-expand" onClick={toggleExpand}>
-          {isExpanded ? <FaArrowUp /> : <FaArrowDown />}
+          {isExpanded ? "▲" : "▼"}
         </button>
       </div>
-      
       {isExpanded && (
         <div className="instrucao-extra">
           {instrucoes.slice(1).map((instrucao, index) => (
-            <div key={index} className="instrucao-item">
-              {instrucao}
-            </div>
+            <p key={index} className="instrucao-item">{instrucao}</p>
           ))}
         </div>
       )}
